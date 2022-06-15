a8429763e3d9a0a45d88b54436e6d9ba6a72f231
vmluweb <vina.marieluce@gmail.com>
2022-06-15 16:56:39 +0200

    source

--- index.php
+++ index.php
@@ -27,6 +27,6 @@ session_start();
 <?php include("layout/footer.php"); ?>
 <!-- Fin du menu -->
 <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="script.js"></script>
 </body>
 </html>
